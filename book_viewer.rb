7bb37933d8072f0614a73ea9020226c7ba69b6a2
vonnenaut <danielashcom@gmail.com>
2020-04-07 21:54:36 +0700

    u/d Gemfile.lock, bookviewer.rb and added views/home.erb

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,6 +1,10 @@
+require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @title = 'The Adventures of Sherlock Holmes'
+  @contents = File.readlines("data/toc.txt", chomp: true)
+
+  erb :home
 end
