7f41514bc6003c26fa6835a21833bb36c1513858
vonnenaut <danielashcom@gmail.com>
2020-04-09 08:04:42 +0700

    u/d book_viewer, views docs

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,17 +2,36 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
-  @contents = File.readlines("data/toc.txt", chomp: true)
 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = 'Chapter 1'
-  @contents = File.readlines("data/toc.txt", chomp: true)
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+not_found do
+  redirect "/"
 end
