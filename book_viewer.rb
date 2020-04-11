6135dd0d3f4a69ac894e06f7809587af7f3570c3
vonnenaut <danielashcom@gmail.com>
2020-04-11 08:35:07 +0700

    u/d book_viewer.rb and search.erb

--- book_viewer.rb
+++ book_viewer.rb
@@ -8,9 +8,13 @@ end
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
     end.join
+  end
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
   end
 end
 
@@ -32,6 +36,40 @@ get "/chapters/:number" do
   erb :chapter
 end
 
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
 not_found do
   redirect "/"
+end
+
+def each_chapter
+  @contents.each_with_index do |name,index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name, :number, and
+# :paragraphs keys. The value for :paragraphs will be a hash of paragraph indices
+# and that paragraph's text.
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
 end
